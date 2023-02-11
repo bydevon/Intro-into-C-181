6c555630d290744f2ddbc41b2459eee142c1fcbe
bydevon <74090462+bydevon@users.noreply.github.com>
2023-02-11 03:25:09 -0800

    Update Lab3Exercise4.cpp

--- Lab 03/Lab3Exercise4/Lab3Exercise4.cpp
+++ Lab 03/Lab3Exercise4/Lab3Exercise4.cpp
@@ -6,8 +6,8 @@ Developer: Devon Ellison
 *************************************************************************/
 
 /*
-Write a program that will convert U.S.dollar amounts to Japanese yenand to euros, storing the conversion factors in the
-constants YEN_PER_DOLLAR and EUROS_PER_DOLLAR. Use the following exchange rates
+Write a program that will convert U.S.dollar amounts to Japanese yen and to euros, storing the conversion factors in the
+constants YEN_PER_DOLLAR and EUROS_PER_DOLLAR. Use the following exchange rates:
 
 1 Dollar = 98.93 Yen
 
@@ -27,15 +27,14 @@ int main()
     cin >> dollar;
 
     // conversion rates given
-    double YEN_PER_DOLLAR = dollar * 98.93;
-    double EUROS_PER_DOLLAR = dollar * .74;
+    const double YEN_PER_DOLLAR = dollar * 98.93;
+    const double EUROS_PER_DOLLAR = dollar * .74;
 
     // I put the converted amounts line in for the setprecision to be out of the converted lines
     cout << "---------Converted Amounts---------" << setprecision(2) << fixed << endl;
     cout << dollar << " Dollars converts to: " << YEN_PER_DOLLAR << " in Yen" << endl;
     cout << dollar << " Dollars converts to: " << EUROS_PER_DOLLAR << " in Euros";
     
-
     return 0;
 }
 
